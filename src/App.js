ae53ece623a6818918b2766809d7a02c43cdd77a
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-11-10 12:04:14 +0100

    working functionalyty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+import ProjectList from './components/projects/ProjectList';
+import { Switch, Route } from 'react-router-dom';
+import TaskDetails from './components/tasks/TaskDetails';
+
+
+import Navbar from './components/navbar/Navbar';
+import ProjectDetails from './components/projects/ProjectDetails';
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <div className="App">
+        <Navbar/>
+        <Switch>
+          <Route exact path="/projects" component={ProjectList}/>
+          <Route  exact path="/projects/:id" component={ProjectDetails} />
+          <Route exact path="/projects/:id/tasks/:taskId" component={TaskDetails} /> {/* <== !!! */}
+        </Switch>
+      </div>
+    );
+  }
+
 }
+ 
+
 
 export default App;
